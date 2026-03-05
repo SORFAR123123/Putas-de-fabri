50b94cf0cf8850df275799c1e7a59c2db12e3187
SORFAR123123 <nino12171f@gmail.com>
2026-03-05 14:14:27 -0500

    Update galeria.js

--- galeria.js
+++ galeria.js
@@ -204,7 +204,9 @@ const galeriaDatabase = {
              { id: 12, url: "https://pbs.twimg.com/media/HCgdpGGbYAA84Y0?format=png&name=small" },
               { id: 13, url: "https://pbs.twimg.com/media/HCe-aKjawAADi3N?format=jpg&name=small" },
                { id: 14, url: "https://pbs.twimg.com/media/HCqB5bFXgAAjgl9?format=png&name=small" },
-               { id: 15, url: "https://pbs.twimg.com/media/HCqB91TWoAAy3Dt?format=png&name=small" }
+               { id: 15, url: "https://pbs.twimg.com/media/HCqB91TWoAAy3Dt?format=png&name=small" },
+              { id: 16, url: "https://pbs.twimg.com/media/HCq3vdHXYAEffD7?format=png&name=small" }
+              { id: 17, url: "https://pbs.twimg.com/media/HCq3uDLW4AA9bKm?format=png&name=small" }
             
         ]
  
