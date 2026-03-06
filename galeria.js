04e3287b38a5e58c8e28d25a3f686335c327ad1f
SORFAR123123 <nino12171f@gmail.com>
2026-03-05 19:13:16 -0500

    Agregar imagen id 9 a seccion 8_1

--- galeria.js
+++ galeria.js
@@ -377,7 +377,9 @@ const galeriaDatabase = {
         
             { id: 7, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772755901844.jpg" },
         
-            { id: 8, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772755909972.jpg" }
+            { id: 8, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772755909972.jpg" },
+        
+            { id: 9, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772755994925.jpg" }
         ]
     },
     
