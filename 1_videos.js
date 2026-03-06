47ea406f9b1c437f231b46640c64278414d7b101
SORFAR123123 <nino12171f@gmail.com>
2026-03-06 17:06:20 -0500

    Update 1_videos.js

--- 1_videos.js
+++ 1_videos.js
@@ -1001,14 +1001,7 @@ function existeVideo(contenedor, subcontenedor) {
 }
 
 function obtenerContenedoresDisponibles() {
-    const contenedores = {    '4_9': {
-        titulo: "2b culona",
-        driveId: "11LQ-kJTu8puVXCOa0wGw62Gvyt04nL2f",
-        descripcion: "rico culo",
-        timestamps: [],
-        categoria: "Basico"
-    },
-};
+    const contenedores = {};
     
     Object.keys(videosDatabase).forEach(key => {
         const [contenedor, subcontenedor] = key.split('_');
