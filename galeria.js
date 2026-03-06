551bff3ab28405c4b5d1716b54b2e8e317f8f847
SORFAR123123 <nino12171f@gmail.com>
2026-03-06 18:58:20 -0500

    Agregar imagen id 3 a 9_Yotsuba_Itsuki

--- galeria.js
+++ galeria.js
@@ -595,7 +595,9 @@ const galeriaDatabase = {
         imagenes: [
             { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772839260751.jpg" },
         
-            { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772841487324.jpg" }
+            { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772841487324.jpg" },
+        
+            { id: 3, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772841498573.jpg" }
         ]
     },
 
