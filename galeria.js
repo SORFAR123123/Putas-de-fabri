900e3b36bb803310ad87a801be2d98c30a12a7ca
SORFAR123123 <nino12171f@gmail.com>
2026-03-05 19:21:23 -0500

    Agregar imagen id 15 a seccion 1_1

--- galeria.js
+++ galeria.js
@@ -76,7 +76,9 @@ const galeriaDatabase = {
             { id: 11, url: "https://pbs.twimg.com/media/HCrkCODbYAAn1ax?format=jpg&name=small" },
             { id: 12, url: "https://pbs.twimg.com/media/HCrkRxTXEAAKM1K?format=jpg&name=large" },
             { id: 13, url: "https://pbs.twimg.com/media/HCrmd9GXwAA0waZ?format=jpg&name=small" },
-            { id: 14, url: "https://pbs.twimg.com/media/HCrrmjBXwAAb90l?format=jpg&name=medium" }
+            { id: 14, url: "https://pbs.twimg.com/media/HCrrmjBXwAAb90l?format=jpg&name=medium" },
+        
+            { id: 15, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772756482259.jpg" }
         ]
     },
     
