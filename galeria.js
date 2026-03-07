9dd45c75e87c1a428abfbe1f260ef8166d27c05f
SORFAR123123 <nino12171f@gmail.com>
2026-03-07 11:42:32 -0500

    Agregar imagen id 13 a 9_Ichika_Miku

--- galeria.js
+++ galeria.js
@@ -1014,7 +1014,9 @@ const galeriaDatabase = {
         
             { id: 11, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772901700222.jpg" },
         
-            { id: 12, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772901736676.jpg" }
+            { id: 12, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772901736676.jpg" },
+        
+            { id: 13, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772901750890.jpg" }
         ]
     },
     '9_Ichika_Yotsuba': {
