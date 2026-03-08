7ca7542726b1318cc11142115384ce6a57ea7954
SORFAR123123 <nino12171f@gmail.com>
2026-03-07 20:02:29 -0500

    Crear seccion 15_4

--- galeria.js
+++ galeria.js
@@ -1620,6 +1620,13 @@ const galeriaDatabase = {
             { id: 35, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772931601867.jpg" }
         ]
     },
+    '15_4': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
      '16_1': {
         titulo: "PRUEBA",
         descripcion: "Primer subcontenedor de prueba",
