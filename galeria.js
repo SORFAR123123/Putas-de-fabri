44faa98e2682e879a729a9cddb0419dc860c71cf
SORFAR123123 <nino12171f@gmail.com>
2026-03-07 20:11:29 -0500

    Agregar imagen id 1 a 15_6

--- galeria.js
+++ galeria.js
@@ -1680,7 +1680,9 @@ const galeriaDatabase = {
         descripcion: "",
         categoria: "",
         imagen: "",
-        imagenes: []
+        imagenes: [
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772932287733.jpg" }
+        ]
     },
      '16_1': {
         titulo: "PRUEBA",
