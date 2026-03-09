6420035c3d14ca8e77100b34546c1ca6081d86bc
SORFAR123123 <nino12171f@gmail.com>
2026-03-08 21:45:43 -0500

    Agregar imagen 6/20 a 9_Perras_Playeras

--- galeria.js
+++ galeria.js
@@ -1624,7 +1624,8 @@ const galeriaDatabase = {
             { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024306167.png" },
             { id: 3, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024312768.png" },
             { id: 4, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024319627.png" },
-            { id: 5, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024327788.png" }
+            { id: 5, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024327788.png" },
+            { id: 6, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773024334764.png" }
         ]
     },
 
