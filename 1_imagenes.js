eb88de061a9dc75e8b0a4d68e60ae71287f18464
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 12:42:19 -0500

    Manga: agregar '1_9' — cheras san valentin

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -127,6 +127,12 @@ const sistemaDescriptivo = {
                 descripcion: '', 
                 nivel: '' 
             },
+            '1_9': { 
+                nombre: 'Miku nino kacheras',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773078044266.webp', 
+                descripcion: 'kacheras 14', 
+                nivel: '' 
+            },
             
           
           
