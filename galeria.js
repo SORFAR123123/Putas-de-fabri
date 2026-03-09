0ca2a296c1957c5a806371cb8dc1a143138c7a8c
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 10:50:36 -0500

    Agregar imagen 1/17 a 18_3

--- galeria.js
+++ galeria.js
@@ -3347,7 +3347,9 @@ const galeriaDatabase = {
         descripcion: "",
         categoria: "",
         imagen: "",
-        imagenes: []
+        imagenes: [
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773071427582.jpg" }
+        ]
     },
     '19_1': {
         titulo: "Trola",
