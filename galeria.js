a6f22a23c713c2ca58a7ab914dcc5d17dc0ef7d7
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 08:34:57 -0500

    Agregar imagen 2/108 a 6_Miku_putarda

--- galeria.js
+++ galeria.js
@@ -1145,7 +1145,8 @@ const galeriaDatabase = {
         categoria: "",
         imagen: "",
         imagenes: [
-            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773063283289.jpg" }
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773063283289.jpg" },
+            { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773063289541.jpg" }
         ]
     },
 
