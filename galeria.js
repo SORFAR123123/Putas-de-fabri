a339b284e2d04348b8a55d87071c24dd7ea16833
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 09:16:46 -0500

    Agregar imagen 103/119 a 7_2

--- galeria.js
+++ galeria.js
@@ -1427,7 +1427,8 @@ const galeriaDatabase = {
             { id: 99, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065773777.jpg" },
             { id: 100, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065780093.jpg" },
             { id: 101, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065786128.jpg" },
-            { id: 102, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065792284.jpg" }
+            { id: 102, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065792284.jpg" },
+            { id: 103, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773065798431.jpg" }
         ]
     },
     
