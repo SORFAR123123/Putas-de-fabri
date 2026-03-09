57967a554ec332447a95d8e7476dad20b5968d1e
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 12:11:45 -0500

    Manga: agregar '1_9' — Kachera

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -127,6 +127,12 @@ const sistemaDescriptivo = {
                 descripcion: '', 
                 nivel: '' 
             },
+            '1_9': { 
+                nombre: 'Yotsuba cherasa',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773076203318.webp', 
+                descripcion: 'chera', 
+                nivel: '' 
+            },
           
           
             
