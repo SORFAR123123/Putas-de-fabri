c3d062f18ce5e04c4d4d4e3bc056483cb1d5ffd7
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 09:01:08 -0500

    Agregar imagen 90/108 a 6_3

--- galeria.js
+++ galeria.js
@@ -1233,7 +1233,8 @@ const galeriaDatabase = {
             { id: 86, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064836125.jpg" },
             { id: 87, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064842102.jpg" },
             { id: 88, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064848139.jpg" },
-            { id: 89, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064854258.jpg" }
+            { id: 89, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064854258.jpg" },
+            { id: 90, url: "https://raw.githubusercontent.com/SORFAR123123/XDXDXDXDXD/main/imagenes/img_1773064860200.jpg" }
         ]
     },
 
