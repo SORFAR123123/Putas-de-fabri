fb016ecd16dddae392e203227d7f1ba87a27f5fe
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 21:33:47 -0500

    Actualizar titulo/imagen de 6_5

--- galeria.js
+++ galeria.js
@@ -1794,10 +1794,10 @@ const galeriaDatabase = {
     },
 
     '6_5': {
-        titulo: "",
+        titulo: "petera",
         descripcion: "",
-        categoria: "",
-        imagen: "",
+        categoria: "okumix",
+        imagen: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD/main/imagenes/img_1773110022742.png",
         imagenes: [
             { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD/main/imagenes/img_1773109756691.png" },
             { id: 2, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD/main/imagenes/img_1773109763365.png" },
