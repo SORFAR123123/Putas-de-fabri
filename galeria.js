efe7b77191b9a4a61389e06e1d560742b2d2ab11
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 21:38:40 -0500

    Agregar imagen 1/12 a 16_4

--- galeria.js
+++ galeria.js
@@ -3955,7 +3955,9 @@ const galeriaDatabase = {
         descripcion: "",
         categoria: "",
         imagen: "",
-        imagenes: []
+        imagenes: [
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD/main/imagenes/img_1773110316853.jpg" }
+        ]
     },
     '17_1': {
         titulo: "Rias putarda",
