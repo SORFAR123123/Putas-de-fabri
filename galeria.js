88b3448042f8af67ab560c050d81a2c9564ea52b
SORFAR123123 <nino12171f@gmail.com>
2026-03-09 19:41:51 -0500

    Agregar imagen 1/52 a 1_8

--- galeria.js
+++ galeria.js
@@ -518,7 +518,9 @@ const galeriaDatabase = {
         descripcion: "",
         categoria: "",
         imagen: "",
-        imagenes: []
+        imagenes: [
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD/main/imagenes/img_1773103302357.png" }
+        ]
     },
    
     
