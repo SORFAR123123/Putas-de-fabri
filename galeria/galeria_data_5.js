4a92f59ff820b8e8486bf79c459f62ffa6d552f6
SORFAR123123 <nino12171f@gmail.com>
2026-03-12 13:17:02 -0500

    Crear seccion 5_6

--- galeria/galeria_data_5.js
+++ galeria/galeria_data_5.js
@@ -412,6 +412,14 @@ const galeriaData_5 = {
             { id: 176, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD4/main/imagenes/img_1773182087516.jpg" }
         ]
     },
+
+    '5_6': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
     
     // CONTENEDOR 6 - Miku
 };
