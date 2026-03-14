1a012e8de9c26873a7f5a1b3e1d3986faacee7e0
SORFAR123123 <nino12171f@gmail.com>
2026-03-14 12:29:00 -0500

    Crear seccion 1_24

--- galeria/galeria_data_1.js
+++ galeria/galeria_data_1.js
@@ -1451,6 +1451,14 @@ const galeriaData_1 = {
             { id: 16, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD12/main/imagenes/img_1773506158792.png" }
         ]
     },
+
+    '1_24': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
    
     
     // CONTENEDOR 2
