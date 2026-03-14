97ca7cd449f05545f5305d28cf7dec88f086d573
SORFAR123123 <nino12171f@gmail.com>
2026-03-14 15:45:17 -0500

    Agregar imagen a 22_1

--- galeria/galeria_data_22.js
+++ galeria/galeria_data_22.js
@@ -5,7 +5,9 @@ const galeriaData_22 = {
         descripcion: "",
         categoria: "",
         imagen: "https://pbs.twimg.com/media/HDZfZxmX0AE5irg?format=png&name=small",
-        imagenes: []
+        imagenes: [
+            { id: 1, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1773521110142.png" }
+        ]
     }
 };
 if (typeof galeriaDatabase !== 'undefined') {
