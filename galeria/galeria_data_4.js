294509b129c1346d9a6da1c6d68f6ebac8f223af
SORFAR123123 <nino12171f@gmail.com>
2026-03-14 11:30:06 -0500

    Crear seccion 4_10

--- galeria/galeria_data_4.js
+++ galeria/galeria_data_4.js
@@ -729,6 +729,14 @@ const galeriaData_4 = {
             { id: 18, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD11/main/imagenes/img_1773503755680.png" }
         ]
     },
+
+    '4_10': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
     
     // CONTENEDOR 5 - Ichika
 };
