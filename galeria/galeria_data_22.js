0d764940cf855ab6fb0cee51dc55ea5cc898af42
SORFAR123123 <nino12171f@gmail.com>
2026-03-14 15:28:16 -0500

    Update galeria_data_22.js

--- galeria/galeria_data_22.js
+++ galeria/galeria_data_22.js
@@ -1,8 +1,13 @@
 // GALERÍA - DATOS CHICA 22
-
 const galeriaData_22 = {
+    '22_1': {
+        titulo: "Chizuru",
+        descripcion: "",
+        categoria: "",
+        imagen: "https://tu-url-de-portada...",
+        imagenes: []
+    }
 };
-
 if (typeof galeriaDatabase !== 'undefined') {
     Object.assign(galeriaDatabase, galeriaData_22);
 }
