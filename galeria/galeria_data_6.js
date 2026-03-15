d77372e9c79106452e68e6876351aa2c51fc6fa4
SORFAR123123 <nino12171f@gmail.com>
2026-03-15 18:42:34 -0500

    Agregar imagen a 6_1

--- galeria/galeria_data_6.js
+++ galeria/galeria_data_6.js
@@ -47,7 +47,8 @@ const galeriaData_6 = {
         
             { id: 19, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772978070258.jpg" },
         
-            { id: 20, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772978076818.jpg" }
+            { id: 20, url: "https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772978076818.jpg" },
+            { id: 21, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1773618143917.png" }
         ]
     },
     '6_2': {
