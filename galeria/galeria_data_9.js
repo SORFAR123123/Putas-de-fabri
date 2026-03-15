43acce62732dfb9f1b841d8740bef15c10522479
SORFAR123123 <nino12171f@gmail.com>
2026-03-15 13:53:37 -0500

    Crear seccion 9_peteras_en_al_noche

--- galeria/galeria_data_9.js
+++ galeria/galeria_data_9.js
@@ -510,6 +510,14 @@ const galeriaData_9 = {
             { id: 131, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD20/main/imagenes/img_1773600409041.jpg" }
         ]
     },
+
+    '9_peteras_en_al_noche': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
