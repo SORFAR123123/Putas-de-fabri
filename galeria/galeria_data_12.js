bba08e898eabd4b38550078f8211bcaa73381b20
SORFAR123123 <nino12171f@gmail.com>
2026-03-16 10:02:49 -0500

    Crear seccion 12_Ricas_conchas_peteras

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -235,6 +235,14 @@ const galeriaData_12 = {
             { id: 20, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD19/main/imagenes/img_1773671041658.jpg" }
         ]
     },
+
+    '12_Ricas_conchas_peteras': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
