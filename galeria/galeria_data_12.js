eed8a934afc84445227153c7296c2b4627193b51
SORFAR123123 <nino12171f@gmail.com>
2026-03-16 15:23:21 -0500

    Crear seccion 12_Rica_esttirada_de_ano

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -1083,6 +1083,14 @@ const galeriaData_12 = {
             { id: 15, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773691949521.jpg" }
         ]
     },
+
+    '12_Rica_esttirada_de_ano': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
