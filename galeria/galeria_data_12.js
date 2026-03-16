79005264ef66a8c3ec084b798b4722a4b2bd0c26
SORFAR123123 <nino12171f@gmail.com>
2026-03-16 14:10:24 -0500

    Crear seccion 12_sentadas

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -635,6 +635,14 @@ const galeriaData_12 = {
             { id: 81, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD20/main/imagenes/img_1773688018480.jpg" }
         ]
     },
+
+    '12_sentadas': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
