f14de19e2fd58b029d57dde8380d91d0ed93c0da
SORFAR123123 <nino12171f@gmail.com>
2026-03-17 09:17:38 -0500

    Crear seccion 12_anal

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -1244,6 +1244,14 @@ const galeriaData_12 = {
             { id: 46, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773756853515.jpg" }
         ]
     },
+
+    '12_anal': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
