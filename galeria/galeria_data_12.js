aa1f32e9429a179e2e1a6abb872f133be81de2c7
SORFAR123123 <nino12171f@gmail.com>
2026-03-17 11:53:19 -0500

    Crear seccion 12_DeepThroat

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -1491,6 +1491,14 @@ const galeriaData_12 = {
             { id: 32, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD21/main/imagenes/img_1773763610760.jpg" }
         ]
     },
+
+    '12_DeepThroat': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
