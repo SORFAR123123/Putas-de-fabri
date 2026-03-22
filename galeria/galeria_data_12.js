2ce8ba898f94f47920ca03a5e558c49a0c6701b7
SORFAR123123 <nino12171f@gmail.com>
2026-03-22 11:49:42 -0500

    Crear seccion 12_Hasta_el_fondo

--- galeria/galeria_data_12.js
+++ galeria/galeria_data_12.js
@@ -1598,6 +1598,14 @@ const galeriaData_12 = {
             { id: 8, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD22/main/imagenes/img_1773775824665.png" }
         ]
     },
+
+    '12_Hasta_el_fondo': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
