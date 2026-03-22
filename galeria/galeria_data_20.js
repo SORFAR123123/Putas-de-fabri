531a46b93da347f43f122c5f8fa368e70606eac9
SORFAR123123 <nino12171f@gmail.com>
2026-03-22 11:57:03 -0500

    Crear seccion 20_6

--- galeria/galeria_data_20.js
+++ galeria/galeria_data_20.js
@@ -201,6 +201,14 @@ const galeriaData_20 = {
             { id: 34, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD2/main/imagenes/img_1773162641546.png" }
         ]
     },
+
+    '20_6': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
      '21_1': {
         titulo: "PRUEBA",
         descripcion: "Primer subcontenedor de prueba",
