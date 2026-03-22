6f6666e69be39f21e005d2ac5d8a73fd42d06440
SORFAR123123 <nino12171f@gmail.com>
2026-03-22 12:08:18 -0500

    Agregar 1 imagen a 1_1

--- galeria/galeria_data_1.js
+++ galeria/galeria_data_1.js
@@ -255,7 +255,8 @@ const galeriaData_1 = {
             { id: 149, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774109701266_2.png" },
             { id: 150, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774109702956_3.png" },
             { id: 151, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774109704579_4.png" },
-            { id: 152, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774109706095_5.png" }
+            { id: 152, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774109706095_5.png" },
+            { id: 153, url: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774199293859_1.png" }
         ]
     },
     '1_2': {
