2deeea9e5739bb973c8e0f243742884e6cb7b97d
SORFAR123123 <nino12171f@gmail.com>
2026-03-24 10:53:18 -0500

    Agrega imagen 7_13: Nino kachando

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2220,7 +2220,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
              
-             '8_1': { 
+            '7_13': { 
+                nombre: 'Nino kachando',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_4440_1774367566582.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                         '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
