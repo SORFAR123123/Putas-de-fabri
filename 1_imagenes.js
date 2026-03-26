416374957787772a313907675e6ea23128c1d8e7
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:08:45 -0500

    Agrega imagen 7_18: Chizuru petera

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2289,7 +2289,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                         '8_1': { 
+            '7_18': { 
+                nombre: 'Chizuru petera',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_5460_1774483573185.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                     '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
