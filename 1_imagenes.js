07aa3798583115d6713d6923fd32f16b63177187
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:35:03 -0500

    Agrega imagen 7_25: Chupadora

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2338,7 +2338,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                             '8_1': { 
+            '7_25': { 
+                nombre: 'Chupadora',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_9900_1774485238162.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                         '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
