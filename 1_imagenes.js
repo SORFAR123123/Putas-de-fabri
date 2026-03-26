f7b4c49fb2a013bf932e73b28854bdca42f0b9cc
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 11:16:27 -0500

    Agrega imagen 7_24: Putonas

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2335,7 +2335,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                          '8_1': { 
+            '7_24': { 
+                nombre: 'Putonas',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_34870_1774541741294.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                      '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
