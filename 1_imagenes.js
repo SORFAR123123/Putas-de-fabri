07a7af8245581d948e5bbf09cd504e31f7dc10bd
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 10:59:21 -0500

    Agrega imagen 7_18: mamada 2

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2293,7 +2293,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                  '8_1': { 
+            '7_18': { 
+                nombre: 'mamada 2',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_5130_1774540733936.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                              '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
