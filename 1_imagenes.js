c737206c7697c69c2b80ea1043b6d015c2773ba5
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:24:55 -0500

    Agrega imagen 7_21: Nobara peteando

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2310,7 +2310,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                             '8_1': { 
+            '7_21': { 
+                nombre: 'Nobara peteando',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_1540_1774484626168.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                         '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
