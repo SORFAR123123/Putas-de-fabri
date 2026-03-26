5e20610622108642d53ee2b2a4fa81ff1b0e0378
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:22:24 -0500

    Agrega video 7_20: Alya petera

--- 1_videos.js
+++ 1_videos.js
@@ -1187,7 +1187,18 @@ const videosDatabase = {
         categoria: "Basico"
     },
 
-                                  '8_1': {
+
+    // Sub-contenedor 7_20
+    '7_20': {
+        titulo: "Alya petera",
+        driveId: "1P-bm9dH2VLh26ucQVZP0X93ZYE2RoMo7",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "Basico"
+    },
+
+                                      '8_1': {
         titulo: "Fabri putardo novia yotsuba potona",
         driveId: "1ZOppS-j8KFYmDncgh4A1T9t1Q77tbKS7", // zero two
         descripcion: "Descuide a Yotsuba",
