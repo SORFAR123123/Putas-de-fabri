ff6e91bafe16c4efd50f6d406f94301ecd124c0a
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:33:35 -0500

    Agrega imagen 7_24: Oyakodon

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2331,7 +2331,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                 '8_1': { 
+            '7_24': { 
+                nombre: 'Oyakodon',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_3940_1774485073029.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                             '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
