8bf6db65d5c45f94322cebe70ebaa4b56ebd3ad2
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 10:58:27 -0500

    Agrega imagen 7_17: petera

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2286,7 +2286,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                      '8_1': { 
+            '7_17': { 
+                nombre: 'petera',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_3220_1774540580706.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                  '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
