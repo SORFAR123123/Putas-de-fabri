85750a99127e5860d1144ce2b534806383d98881
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 11:10:50 -0500

    Agrega imagen 7_22: Petera de bolas

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2321,7 +2321,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                  '8_1': { 
+            '7_22': { 
+                nombre: 'Petera de bolas',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_10880_1774541386364.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                              '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
