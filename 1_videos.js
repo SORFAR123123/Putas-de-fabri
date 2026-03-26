c109b31e6f535f7837c0bcce8383d9f9cc595b8b
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 10:58:25 -0500

    Agrega video 7_17: petera

--- 1_videos.js
+++ 1_videos.js
@@ -1157,7 +1157,18 @@ const videosDatabase = {
         categoria: "Basico"
     },
 
-                     '8_1': {
+
+    // Sub-contenedor 7_17
+    '7_17': {
+        titulo: "petera",
+        driveId: "1IlBr0M5WjU_h_QbPBVmUgXdOvqIotzks",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "Basico"
+    },
+
+                         '8_1': {
         titulo: "Fabri putardo novia yotsuba potona",
         driveId: "1ZOppS-j8KFYmDncgh4A1T9t1Q77tbKS7", // zero two
         descripcion: "Descuide a Yotsuba",
