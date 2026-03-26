cb9c590144c120b80e5ce36574e6d1709ede6788
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 17:49:00 -0500

    Agrega imagen 7_29: Kachando a jean

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2379,7 +2379,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                                                                  '8_1': { 
+            '7_29': { 
+                nombre: 'Kachando a jean',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_4620_1774564987615.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                                                              '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
