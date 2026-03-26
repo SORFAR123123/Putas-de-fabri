4a3b94216b011cc42c4bd514abfc4c05f9341cc8
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:10:36 -0500

    Agrega video 7_19: Robin petera

--- 1_videos.js
+++ 1_videos.js
@@ -1176,7 +1176,18 @@ const videosDatabase = {
         categoria: "Basico"
     },
 
-                              '8_1': {
+
+    // Sub-contenedor 7_19
+    '7_19': {
+        titulo: "Robin petera",
+        driveId: "1dSwpAZplEOllLfRktXoR-wTcObR9YA_W",
+        descripcion: "",
+        timestamps: [],
+        duracion: "0:00",
+        categoria: "Basico"
+    },
+
+                                  '8_1': {
         titulo: "Fabri putardo novia yotsuba potona",
         driveId: "1ZOppS-j8KFYmDncgh4A1T9t1Q77tbKS7", // zero two
         descripcion: "Descuide a Yotsuba",
