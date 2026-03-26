4d22b6ccfb5a910f0f016c757755712120d3d360
SORFAR123123 <nino12171f@gmail.com>
2026-03-26 16:01:02 -0500

    Agrega imagen 7_27: Kachando con kafka

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2363,7 +2363,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                              '8_1': { 
+            '7_27': { 
+                nombre: 'Kachando con kafka',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_14640_1774557556038.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                          '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
