6b024a8271e243449264f0cb6de0362c49ba2c83
SORFAR123123 <nino12171f@gmail.com>
2026-03-25 19:36:26 -0500

    Agrega imagen 7_26: Yor kachera

--- 1_imagenes.js
+++ 1_imagenes.js
@@ -2345,7 +2345,14 @@ const sistemaDescriptivo = {
                 duracion: '' 
             },
             
-                                                                                                                                                                         '8_1': { 
+            '7_26': { 
+                nombre: 'Yor kachera',
+                imagen: 'https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/frame_7830_1774485319621.webp', 
+                descripcion: '', 
+                duracion: '' 
+            },
+            
+                                                                                                                                                                                     '8_1': { 
                 nombre: 'fabri putardo',
                 imagen: 'https://pbs.twimg.com/media/G_2dDpBX0AAcGz6?format=png&name=small', 
                 descripcion: '', 
