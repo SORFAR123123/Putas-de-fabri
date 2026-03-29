547c962fb58c03b3a0b17989e62f72fbba35d7fa
SORFAR123123 <nino12171f@gmail.com>
2026-03-29 13:23:25 -0500

    Crear seccion 6_24

--- galeria/galeria_data_6.js
+++ galeria/galeria_data_6.js
@@ -1643,6 +1643,14 @@ const galeriaData_6 = {
         ]
     },
 
+    '6_24': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
+
    
 
   
