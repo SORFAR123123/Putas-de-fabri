7eb61e2ab7a357fec75337a9341ba43ccca5aa19
SORFAR123123 <nino12171f@gmail.com>
2026-03-29 12:56:45 -0500

    Crear seccion 9_Yotsuba_Nino_putitas

--- galeria/galeria_data_9.js
+++ galeria/galeria_data_9.js
@@ -838,6 +838,14 @@ const galeriaData_9 = {
             { id: 30, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD22/main/imagenes/img_1774105153044.jpg" }
         ]
     },
+
+    '9_Yotsuba_Nino_putitas': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
