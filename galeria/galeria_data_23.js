c73011aa55b25fd636a02229ad0fef0fcc5932a5
SORFAR123123 <nino12171f@gmail.com>
2026-03-30 18:34:25 -0500

    Crear seccion 23_3

--- galeria/galeria_data_23.js
+++ galeria/galeria_data_23.js
@@ -64,6 +64,14 @@ const galeriaData_23 = {
             { id: 47, url: "https://raw.githubusercontent.com/SORFAR123123/XDDDDDDDDDDDDDDDDDDDDXDXDXDXDXDXD28/main/imagenes/img_1774913417150.jpg" }
         ]
     },
+
+    '23_3': {
+        titulo: "",
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        imagenes: []
+    },
 };
 
 if (typeof galeriaDatabase !== 'undefined') {
