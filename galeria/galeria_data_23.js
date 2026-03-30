b09981eb23f2c507861c55eed7a327cad36b12ff
SORFAR123123 <nino12171f@gmail.com>
2026-03-30 18:16:37 -0500

    Crear data #23: Nobara

---
+++ galeria/galeria_data_23.js
@@ -0,0 +1,15 @@
+// GALERÍA - DATOS CHICA 23: Nobara
+
+const galeriaData_23 = {
+    '23_1': {
+        titulo: "Nobara",
+        descripcion: "",
+        categoria: "Nobara",
+        imagen: "https://raw.githubusercontent.com/SORFAR123123/WAIFUAJUssola/main/imagenes/img_1774912583173.jpg",
+        imagenes: []
+    }
+};
+
+if (typeof galeriaDatabase !== 'undefined') {
+    Object.assign(galeriaDatabase, galeriaData_23);
+}
